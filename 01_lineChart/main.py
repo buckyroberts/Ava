d4a41be08c76e6be9c1590c0d4865f6414c12ca0
Bucky <buckyroberts@gmail.com>
2016-05-14 15:38:13 -0400

    Added sample charts

---
+++ 01_lineChart/main.py
@@ -0,0 +1,8 @@
+from matplotlib import pyplot as plt
+
+age = [1, 2, 3, 4, 5]
+iq = [14, 25, 44, 78, 139]
+
+plt.plot(age, iq)
+plt.title('Average IQ of Babies')
+plt.show()
--- main.py
+++
@@ -1,1 +0,0 @@
-print('Hello Cruel World!')
